6ae907d47e5eca402251f1aa1753a39d6143eea6
Shanti-metal <124040296+Shanti-metal@users.noreply.github.com>
2023-02-07 09:21:23 +0000

    login design

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { Button, Col, Container, Form } from "react-bootstrap";
+import './Login.scss';
+import { SiFacebook } from 'react-icons/si';
+import { FcGoogle } from 'react-icons/fc';
 
-function Login(){
-    return(
-        <>
-            <h1>This is a Login Page</h1>
-        </>
+function Login() {
+    return (
+        <Container className="login-form">
+            <div className="page-header">
+                <h1>Login</h1>
+                <p>Please enter your e-mail and password:</p>
+                <div className="social-icon">
+                    <SiFacebook />
+                    <FcGoogle />
+                </div>
+            </div>
+ 
+            <div className="page-form">
+                <Form>
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <Button className="loginBtn">Login</Button>
+                    <p>or</p>
+                    <Button className="regBtn">Register</Button>
+                    <ul>
+                        <li>New Customer?</li>
+                        <li>Create an account</li>
+                    </ul>
+                </Form>
+            </div>
+        </Container>
     )
 }
 export default Login;
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { Button, Col, Container, Form } from "react-bootstrap";
+import './Login.scss';
+import { SiFacebook } from 'react-icons/si';
+import { FcGoogle } from 'react-icons/fc';
 
 function Signup(){
     return(
-        <>
-            <h1>This is a Signup Page</h1>
-        </>
+        <Container className="login-form">
+            <div className="page-header">
+                <h1>Sign Up</h1>
+                <p>Please enter your e-mail and password:</p>
+                <div className="social-icon">
+                    <SiFacebook />
+                    <FcGoogle />
+                </div>
+            </div>
+ 
+            <div className="page-form">
+                <Form>
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <Button>Login</Button>
+                    <ul>
+                        <li>New Customer?</li>
+                        <li>Create an account</li>
+                    </ul>
+                </Form>
+            </div>
+        </Container>
     )
 }
 export default Signup;
